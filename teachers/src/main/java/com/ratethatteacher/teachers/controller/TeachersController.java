a2845404f9b438b9baeb09e6dd8c05a6e1d36f24
Cristian Petroaca <cristian.petroaca@gmail.com>
2023-01-28 21:45:11 +0200

    Added Spring scaffolding

---
+++ teachers/src/main/java/com/ratethatteacher/teachers/controller/TeachersController.java
@@ -0,0 +1,12 @@
+package com.ratethatteacher.teachers.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeachersController {
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
